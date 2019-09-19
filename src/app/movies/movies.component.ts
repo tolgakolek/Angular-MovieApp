b7ead3cfdebfd42e18ee63a879421b298264d8be
tolgakolek <tolgakolek@gmail.com>
2019-09-19 15:00:04 +0300

    Lesson 1: Generating Component

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'movies', // <movies></movies>
+    templateUrl: 'movies.component.html'
+})
+export class MoviesComponent {
+
+}
